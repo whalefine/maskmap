0bf72a74eb542c529fd42ed01e370ebb6f985f67
whale_fine <whfjgir325@gmail.com>
2022-04-10 14:49:01 +0800

    add loading component

---
+++ src/components/CircleLoading.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="circle-loading">
+    <div class="circle-loading__circle"></div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+.circle-loading {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  z-index: 9999;
+  background: rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &__circle {
+    width: 70px;
+    height: 70px;
+    border: 5px solid rgb(209, 209, 209);
+    border-top: 5px solid black;
+    border-radius: 50%;
+    animation: spin linear 3s infinite;
+  }
+}
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
